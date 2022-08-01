c5e14368cd7c47578a692ce7b4cda90ae755111c
NahrinMalakes <nahrinmalakes12@gmail.com>
2022-08-01 18:06:27 +0300

    Maybe optimized barcode scanning

--- src/pages/product/create.tsx
+++ src/pages/product/create.tsx
@@ -74,15 +74,18 @@ const CreateProduct: NextPage = () => {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
             <div className="mt-4 mb-4">
-              <BarCodeScanner
-                onUpdate={(err, res) => {
-                  if (err) {
-                    console.log(err);
-                  } else if (res && res.getText()) {
-                    setBarcode(res.getText());
-                  }
-                }}
-              />
+              {!stop && (
+                <BarCodeScanner
+                  onUpdate={(err, res) => {
+                    if (err) {
+                      console.log(err);
+                    } else if (res && res.getText()) {
+                      setBarcode(res.getText());
+                      setStop(true);
+                    }
+                  }}
+                />
+              )}
             </div>
             <div className="">
               <button
