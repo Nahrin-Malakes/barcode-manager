f04d4df52319d5649bf1c59f665c6d653c78aa2e
NahrinMalakes <nahrinmalakes12@gmail.com>
2022-08-03 13:17:02 +0300

    centerd card, replace button's text with icons, added "show barcode" function

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,11 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { trpc } from "../utils/trpc";
+// @ts-ignore
+import Barcode from "react-barcode";
+import { BsFillTrashFill } from "react-icons/bs";
+import { FiEdit } from "react-icons/fi";
+import { BiBarcodeReader } from "react-icons/bi";
 
 interface Props {
   name: string;
@@ -33,7 +38,7 @@ export const ProductCard = ({ name, barcode, price }: Props) => {
           type={"submit"}
           onClick={() => setShowModal(true)}
         >
-          Delete
+          <BsFillTrashFill />
         </button>
         {showModal ? (
           <>
@@ -99,16 +104,16 @@ export const ProductCard = ({ name, barcode, price }: Props) => {
     return (
       <>
         <button
-          className="text-white mt-2 cursor-pointer bg-blue-700 hover:bg-blue-800 mx-4 focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-700 dark:hover:bg-blue-800"
+          className="text-white mt-2 cursor-pointer bg-blue-700 hover:bg-blue-800 mx-2 focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-700 dark:hover:bg-blue-800"
           type={"submit"}
           onClick={() => setShowModal(true)}
         >
-          Edit
+          <FiEdit />
         </button>
         {showModal ? (
           <>
             <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-              <div className="relative w-auto my-6 mx-auto max-w-xl">
+              <div className="relative w-96 mx-auto max-w-xl">
                 {/*content*/}
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-700 outline-none focus:outline-none">
                   {/*header*/}
@@ -120,9 +125,6 @@ export const ProductCard = ({ name, barcode, price }: Props) => {
                   </div>
                   {/*body*/}
                   <div className="relative p-6 flex-auto">
-                    <h1 className="my-4 text-gray-300 text-2xl leading-relaxed">
-                      Are you sure you want to delete this product?
-                    </h1>
                     <form
                       onSubmit={(e) => {
                         e.preventDefault();
@@ -184,8 +186,59 @@ export const ProductCard = ({ name, barcode, price }: Props) => {
     );
   };
 
+  const ShowBarcodeModal = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [editName, setEditName] = useState(name);
+    const [editPrice, setEditPrice] = useState(price);
+
+    return (
+      <>
+        <button
+          className="text-white mt-2 cursor-pointer bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-700 dark:hover:bg-blue-800"
+          type={"submit"}
+          onClick={() => setShowModal(true)}
+        >
+          <BiBarcodeReader />
+        </button>
+        {showModal ? (
+          <>
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+              <div className="relative w-auto my-6 mx-auto max-w-xl">
+                {/*content*/}
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-700 outline-none focus:outline-none">
+                  {/*header*/}
+                  <div className="flex items-start justify-between  border-slate-200 rounded-t">
+                    <button
+                      className="p-1 ml-auto bg-transparent border-0 opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                      onClick={() => setShowModal(false)}
+                    ></button>
+                  </div>
+                  {/*body*/}
+                  <div className="relative p-6 flex-auto">
+                    <Barcode value={barcode} />
+                  </div>
+                  {/*footer*/}
+                  <div className="flex items-center justify-center p-6 border-t border-solid border-slate-200 rounded-b">
+                    <button
+                      type="button"
+                      onClick={() => setShowModal(false)}
+                      className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          </>
+        ) : null}
+      </>
+    );
+  };
+
   return (
-    <div className="max-w-sm bg-white flex rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-sm bg-white flex rounded-lg shadow-md dark:bg-gray-800 justify-center dark:border-gray-700">
       <div className="px-5 pb-5">
         <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mt-4">
           Name: {name}
@@ -200,6 +253,7 @@ export const ProductCard = ({ name, barcode, price }: Props) => {
         </div>
         <DeleteProductModal />
         <EditProductModal />
+        <ShowBarcodeModal />
       </div>
     </div>
   );
